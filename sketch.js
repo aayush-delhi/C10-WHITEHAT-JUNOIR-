1cc9e38f11f8ccd42b74f99b158c2e388a28cbfb
aayush-delhi <111556402+aayush-delhi@users.noreply.github.com>
2022-08-27 15:04:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,4 @@
+console.log("HERE IS SOME LINES ABOUT ME ");
+console.log("My names is aayush jyoti from delhi");
+console.log("I study in 7 and school name is N.K Barodia Global School");
+console.log("I LOVE CODDING VERY MUCH")
